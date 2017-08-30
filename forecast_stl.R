486fbcd7e5320860104718ac6924093b166fe4f4
ndricca <andrea.ricca@hotmail.it>
2017-08-31 01:51:50 +0200

    added tbats model and animation

--- forecast_stl.R
+++ forecast_stl.R
@@ -14,7 +14,6 @@ library(tidyverse)
 library(padr)
 library(grid)
 library(animation)
-library(reshape2)
 
 ########### data import and preparation ########### 
 
@@ -77,7 +76,7 @@ days_ahead <- 2
 # head(outdata)
 
 ########### forecast.stl model function ###########
-CtreeTrend <- function(df, set_of_date, period = 24){
+forecast_stl <- function(df, set_of_date, period = 24){
   
   data_train <- df[df$day %in% set_of_date,]
   N <- nrow(data_train)
@@ -94,54 +93,22 @@ CtreeTrend <- function(df, set_of_date, period = 24){
 }
 
 
-############# outlier oneshot ############# 
-# ctree_list <- CtreeTrend(df, n_date[(1):(7*3)])
-# summary(ctree_list)
+#############
+# forecast_stl_list <- forecast_stl(df, n_date[(1):(7*3)])
+# summary(forecast_stl_list)
 # test_data <- df[df$day %in% n_date[22], ]
 # 
 # outliers <- rep(NA,nrow(test_data))
-# outliers[test_data$sum_error > ctree_list$forecast + ctree_list$upper] <- test_data[test_data$sum_error  > ctree_list$forecast + ctree_list$upper,"sum_error"]
+# outliers[test_data$sum_error > forecast_stl_list$upper] <- test_data[test_data$sum_error  > forecast_stl_list$upper,"sum_error"]
 # 
-# data_for <- data.table(Load = c(ctree_list$real, test_data$sum_error, ctree_list$forecast, ctree_list$lower, ctree_list$upper),
+# data_for <- data.table(Load = c(forecast_stl_list$real, test_data$sum_error, forecast_stl_list$forecast, forecast_stl_list$lower, forecast_stl_list$upper),
 #                        Date = c(df[df$day %in% n_date[(i+1):(i+7*3)], "date"],
 #                                 rep(test_data$date, 4)),
-#                        Type = c(rep("Train data", length(ctree_list$real)),
-#                                 rep("Test data", length(ctree_list$forecast)),
-#                                 rep("Fcst mean", length(ctree_list$forecast)),
-#                                 rep("Fcst lower bound", length(ctree_list$forecast)),
-#                                 rep("Fcst upper bound", length(ctree_list$forecast))))
-# 
-# ctree_list <- CtreeTrend(df, n_date[(1):(7*3)])
-# # summary(ctree_list)
-# test_data <- df[df$day %in% n_date[22], ]
-# tail(test_data)
-# outliers <- rep(NA,nrow(test_data))
-# outliers[test_data$sum_error > ctree_list$forecast + ctree_list$upper] <- test_data[test_data$sum_error  > ctree_list$forecast + ctree_list$upper,"sum_error"]
-# outliers[5] <- 200
-# 
-# data_for <- data.table(Load = c(ctree_list$real, test_data$sum_error, ctree_list$forecast, 
-#                                 ctree_list$lower, ctree_list$upper),
-#                        Date = c(df[df$day %in% n_date[(1):(7*3)], "date"],
-#                                 rep(test_data$date, 4)),
-#                        Type = c(rep("Train data", length(ctree_list$real)),
-#                                 rep("Test data", length(ctree_list$forecast)),
-#                                 rep("Fcst mean", length(ctree_list$forecast)),
-#                                 rep("Fcst lower bound", length(ctree_list$forecast)),
-#                                 rep("Fcst upper bound", length(ctree_list$forecast))))
-# outdata <- data.table(Load = outliers,
-#                       Date = test_data$date,
-#                       Type = rep("outliers",nrow(test_data)))
-# 
-# 
-# gg1 <- ggplot(data_for, aes(Date, Load, color = Type)) +
-#   geom_line(size = 0.8, alpha = 0.75)
-# if (!all(is.na(outliers))) gg1 <- gg1 + geom_point(data=outdata,aes(Date,Load),color = "red",shape = "x",size=2.5,na.rm = FALSE)
-# gg1 <- gg1 + facet_zoom(xy = Date %in% test_data$date, zoom.size = 1.2, horizontal = FALSE) +
-#   labs(x= NULL, title =  paste("Forecast from forecast.stl; ", "day number: ", i+21, sep = ""))
-# gg1
-# 
-# 
-# 
+#                        Type = c(rep("Train data", length(forecast_stl_list$real)),
+#                                 rep("Test data", length(forecast_stl_list$forecast)),
+#                                 rep("Fcst mean", length(forecast_stl_list$forecast)),
+#                                 rep("Fcst lower bound", length(forecast_stl_list$forecast)),
+#                                 rep("Fcst upper bound", length(forecast_stl_list$forecast))))
 # 
 # outdata <- data.table(Load = outliers,
 #                       Date = test_data$date,
@@ -157,8 +124,8 @@ CtreeTrend <- function(df, set_of_date, period = 24){
 
 ########### animation ###########
 define_region <- function(row, col){
-    viewport(layout.pos.row = row, layout.pos.col = col)
-  }
+  viewport(layout.pos.row = row, layout.pos.col = col)
+}
 total = n_days-1
 
 pb <- txtProgressBar(min = 0, max = total, style = 3)
@@ -167,64 +134,51 @@ n_days <- floor(length(n_date)) - 21
 
 
 for(i in 0:(n_days-1)){
-
-  ctree_list <- CtreeTrend(df, n_date[(i+1):(i+7*3)])
-
+  
+  forecast_stl_list <- forecast_stl(df, n_date[(i+1):(i+7*3)])
+  
   test_data <- df[df$day %in% n_date[22+i], ]
   n_test_data <- nrow(test_data)
-  if (length(ctree_list$forecast) > n_test_data) {
-    ctree_list$forecast <- ctree_list$forecast[1:n_test_data]
-    ctree_list$lower <- ctree_list$lower[1:n_test_data]
-    ctree_list$upper <- ctree_list$upper[1:n_test_data]
+  if (length(forecast_stl_list$forecast) > n_test_data) {
+    forecast_stl_list$forecast <- forecast_stl_list$forecast[1:n_test_data]
+    forecast_stl_list$lower <- forecast_stl_list$lower[1:n_test_data]
+    forecast_stl_list$upper <- forecast_stl_list$upper[1:n_test_data]
   }
-
-
+  
   outliers <- rep(NA,nrow(test_data))
-  outliers[test_data$sum_error > ctree_list$forecast + ctree_list$upper] <- test_data[test_data$sum_error  > ctree_list$forecast + ctree_list$upper,"sum_error"]
-
-  data_for <- data.table(Load = c(ctree_list$real, test_data$sum_error, ctree_list$forecast, ctree_list$lower, ctree_list$upper),
+  outliers[test_data$sum_error > forecast_stl_list$upper] <- test_data[test_data$sum_error  > forecast_stl_list$upper,"sum_error"]
+  
+  data_for <- data.table(Load = c(forecast_stl_list$real, test_data$sum_error, forecast_stl_list$forecast, forecast_stl_list$lower, forecast_stl_list$upper),
                          Date = c(df[df$day %in% n_date[(i+1):(i+7*3)], "date"],
                                   rep(test_data$date, 4)),
-                         Type = c(rep("Train data", length(ctree_list$real)),
-                                  rep("Test data", length(ctree_list$forecast)),
-                                  rep("Fcst mean", length(ctree_list$forecast)),
-                                  rep("Fcst lower bound", length(ctree_list$forecast)),
-                                  rep("Fcst upper bound", length(ctree_list$forecast))))
-
+                         Type = c(rep("Train data", length(forecast_stl_list$real)),
+                                  rep("Test data", length(forecast_stl_list$forecast)),
+                                  rep("Fcst mean", length(forecast_stl_list$forecast)),
+                                  rep("Fcst lower bound", length(forecast_stl_list$forecast)),
+                                  rep("Fcst upper bound", length(forecast_stl_list$forecast))))
+  
   outdata <- data.table(Load = outliers,
                         Date = test_data$date,
                         Type = rep("outliers",nrow(test_data)))
-
-                        gg1 <- ggplot(data_for, aes(Date, Load, color = Type)) +
-                          geom_line(size = 0.8, alpha = 0.75)
-                        if (!all(is.na(outdata$Load))) gg1 <- gg1 + geom_point(data=outdata,aes(Date,Load,color = Type),color = "red",shape = "x",size=2.5,na.rm = FALSE)
-                        gg1 <- gg1 + facet_zoom(xy = Date %in% test_data$date, zoom.size = 1.2, horizontal = FALSE) +
-                           labs(x= NULL, title =  paste("Forecast from forecast.stl; ", "day number: ", i+21, sep = ""))
-                    #      labs(x= NULL, title =  paste("Forecast from CTREE; ", "day: ", i+21, "; MAPE: ",
-                    #                          round(ctree_err_mape, 2), "%", sep = "")) +
-                    #      theme_ts
-
-                    #    trend_data <- data.table(Load = c(ctree_list$trend, ctree_list$trendFor),
-                    #                             Date = c(DT[date %in% n_date[(i+1):(i+7*3)], date_time],
-                    #                                      test_data$date_time),
-                    #                             Type = c(rep("Real", length(ctree_list$trend)),
-                    #                                      rep("Forecast", length(ctree_list$trendFor))))
-                    #
-                    #    gg2 <- ggplot(trend_data, aes(Date, Load, color = Type)) +
-                    #      geom_line(size = 1.2) +
-                    #      labs(title = paste("Trend Forecasting with ", ctree_list$trendFit)) +
-                    #      theme_ts
-
-                    grid.newpage()
-                    # Create layout : nrow = 2, ncol = 1
-                    pushViewport(viewport(layout = grid.layout(1, 1)))
-                    # Arrange the plots
-                    print(gg1, vp = define_region(1, 1))
-                    #    print(gg2, vp = define_region(2, 1))
-                    setTxtProgressBar(pb, i)
-                    ani.record()
-                    dev.off()
+  
+  gg1 <- ggplot(data_for, aes(Date, Load, color = Type)) +
+    geom_line(size = 0.8, alpha = 0.75)
+  if (!all(is.na(outdata$Load))) gg1 <- gg1 + geom_point(data=outdata,aes(Date,Load,color = Type),color = "red",shape = "x",size=2.5,na.rm = FALSE)
+  gg1 <- gg1 + facet_zoom(xy = Date %in% test_data$date, zoom.size = 1.2, horizontal = FALSE) +
+    labs(x= NULL, title =  paste("Forecast from forecast.stl; ", "day number: ", i+21, sep = ""))
+  
+  grid.newpage()
+  # Create layout : nrow = 2, ncol = 1
+  pushViewport(viewport(layout = grid.layout(1, 1)))
+  # Arrange the plots
+  print(gg1, vp = define_region(1, 1))
+  #    print(gg2, vp = define_region(2, 1))
+  setTxtProgressBar(pb, i)
+  ani.record()
+  dev.off()
 }
 close(pb,i)
+
+imgname = paste0("recorded_plot_",format(Sys.time(),"%Y%m%d%H%M%S"))
 htmlfilename <- paste0("html_animated_plot_",format(Sys.time(),"%Y%m%d%H%M%S"),".html")
-saveHTML(ani.replay(), img.name = "recorded_plot", htmlfile = htmlfilename, outdir = getwd())
+saveHTML(ani.replay(), img.name = imgname, htmlfile = htmlfilename, outdir = getwd())
---
+++ forecast_tbats.R
@@ -0,0 +1,153 @@
+########### opts and packages ########### 
+Sys.setlocale("LC_TIME","C")
+set.seed(123)
+library(graphics)
+library(ggplot2)
+library(ggforce)
+library(rpart)
+library(party)
+library(data.table)
+library(forecast)
+library(tsoutliers)
+library(plotly)
+library(tidyverse)
+library(padr)
+library(grid)
+library(animation)
+
+########### data import and preparation ########### 
+
+df <- read.csv("C://Users//andrea.ricca//Downloads//series1_count_errors.csv",
+               sep = ",",header = T)
+df$date = as.POSIXct(df$date, format = "%Y/%m/%d %H")
+df <- df %>% pad %>% fill_by_value(value = 0)
+levels(df$root) <- "series_one"
+df[is.na(df$root),"root"] <- "series_one"
+df$date_hour <- as.factor(df$date_hour)
+df$date_wday <- as.factor(weekdays(df$date))
+df$day <- format(df$date,"%Y/%m/%d")
+
+########### set parameters ########### 
+
+n_date <- unique(df$day)
+n_days_tot <- floor(length(n_date))
+n_rows <- dim(df)[1]
+period <- 24
+days_ahead <- 2
+
+#### tbats model function ####
+forecast_tbats <- function(df, set_of_date, period = 24){
+  
+  data_train <- df[df$day %in% set_of_date,]
+  data_ts <- ts(data_train$sum_error, freq = period*7)
+  tbats_model <- tbats(data_ts,use.box.cox = TRUE,use.trend = TRUE)
+  
+  fcst <- forecast(tbats_model,h=24,level = 95)
+  fcst_mean <- fcst$mean
+  fcst_lower <- fcst$lower
+  fcst_upper <- fcst$upper
+  
+  return(list(forecast = as.vector(fcst_mean), lower = fcst_lower, upper = fcst_upper,
+              real = data_train$sum_error))
+}
+
+# ###### tbats oneshot model ######
+# i<-11
+# forecast_tbats_list <- forecast_tbats(df, n_date[(i+1):(i+7*3)])
+# 
+# test_data <- df[df$day %in% n_date[22+i], ]
+# n_test_data <- nrow(test_data)
+# if (length(forecast_tbats_list$forecast) > n_test_data) {
+#   forecast_tbats_list$forecast <- forecast_tbats_list$forecast[1:n_test_data]
+#   forecast_tbats_list$lower <- forecast_tbats_list$lower[1:n_test_data]
+#   forecast_tbats_list$upper <- forecast_tbats_list$upper[1:n_test_data]
+# }
+# 
+# outliers <- rep(NA,nrow(test_data))
+# outliers[test_data$sum_error > forecast_tbats_list$upper] <- test_data[test_data$sum_error  > forecast_tbats_list$upper,"sum_error"]
+# 
+# data_for <- data.table(Load = c(forecast_tbats_list$real, test_data$sum_error, 
+#                                 forecast_tbats_list$forecast, forecast_tbats_list$lower, forecast_tbats_list$upper),
+#                        Date = c(df[df$day %in% n_date[(i+1):(i+7*3)], "date"],
+#                                 rep(test_data$date, 4)),
+#                        Type = c(rep("Train data", length(forecast_tbats_list$real)),
+#                                 rep("Test data", length(forecast_tbats_list$forecast)),
+#                                 rep("Fcst mean", length(forecast_tbats_list$forecast)),
+#                                 rep("Fcst lower bound", length(forecast_tbats_list$forecast)),
+#                                 rep("Fcst upper bound", length(forecast_tbats_list$forecast))))
+# 
+# outdata <- data.table(Load = outliers,
+#                       Date = test_data$date,
+#                       Type = rep("outliers",nrow(test_data)))
+# 
+# gg1 <- ggplot(data_for, aes(Date, Load, color = Type)) +
+#   geom_line(size = 0.8, alpha = 0.75)
+# if (!all(is.na(outdata$Load))) gg1 <- gg1 + geom_point(data=outdata,aes(Date,Load,color = Type),color = "red",shape = "x",size=2.5,na.rm = FALSE)
+# gg1 <- gg1 + facet_zoom(xy = Date %in% test_data$date, zoom.size = 1.2, horizontal = FALSE) +
+#   labs(x= NULL, title =  paste("Forecast from forecast.tbats; ", "day number: ", i+21, sep = ""))
+# gg1
+
+
+
+###### animation ########
+define_region <- function(row, col){
+  viewport(layout.pos.row = row, layout.pos.col = col)
+}
+total = n_days-1
+
+pb <- txtProgressBar(min = 0, max = total, style = 3)
+ani.record(reset=TRUE)
+n_days <- floor(length(n_date)) - 21
+
+
+for(i in 0:(n_days-1)){
+  
+  forecast_tbats_list <- forecast_tbats(df, n_date[(i+1):(i+7*3)])
+  
+  test_data <- df[df$day %in% n_date[22+i], ]
+  n_test_data <- nrow(test_data)
+  if (length(forecast_tbats_list$forecast) > n_test_data) {
+    forecast_tbats_list$forecast <- forecast_tbats_list$forecast[1:n_test_data]
+    forecast_tbats_list$lower <- forecast_tbats_list$lower[1:n_test_data]
+    forecast_tbats_list$upper <- forecast_tbats_list$upper[1:n_test_data]
+  }
+  
+  
+  outliers <- rep(NA,nrow(test_data))
+  outliers[test_data$sum_error > forecast_tbats_list$upper] <- test_data[test_data$sum_error  > forecast_tbats_list$upper,"sum_error"]
+  
+  data_for <- data.table(Load = c(forecast_tbats_list$real, test_data$sum_error, forecast_tbats_list$forecast, 
+                                  forecast_tbats_list$lower, forecast_tbats_list$upper),
+                         Date = c(df[df$day %in% n_date[(i+1):(i+7*3)], "date"],
+                                  rep(test_data$date, 4)),
+                         Type = c(rep("Train data", length(forecast_tbats_list$real)),
+                                  rep("Test data", length(forecast_tbats_list$forecast)),
+                                  rep("Fcst mean", length(forecast_tbats_list$forecast)),
+                                  rep("Fcst lower bound", length(forecast_tbats_list$forecast)),
+                                  rep("Fcst upper bound", length(forecast_tbats_list$forecast))))
+  
+  outdata <- data.table(Load = outliers,
+                        Date = test_data$date,
+                        Type = rep("outliers",nrow(test_data)))
+  
+  gg1 <- ggplot(data_for, aes(Date, Load, color = Type)) +
+    geom_line(size = 0.8, alpha = 0.75)
+  if (!all(is.na(outdata$Load))) gg1 <- gg1 + geom_point(data=outdata,aes(Date,Load,color = Type),color = "red",shape = "x",size=2.5,na.rm = FALSE)
+  gg1 <- gg1 + facet_zoom(xy = Date %in% test_data$date, zoom.size = 1.2, horizontal = FALSE) +
+    labs(x= NULL, title =  paste("Forecast from forecast.stl; ", "day number: ", i+21, sep = ""))
+  
+  grid.newpage()
+  # Create layout : nrow = 2, ncol = 1
+  pushViewport(viewport(layout = grid.layout(1, 1)))
+  # Arrange the plots
+  print(gg1, vp = define_region(1, 1))
+  #    print(gg2, vp = define_region(2, 1))
+  setTxtProgressBar(pb, i)
+  ani.record()
+  dev.off()
+}
+close(pb,i)
+
+imgname = paste0("tbats_recorded_plot_",format(Sys.time(),"%Y%m%d%H%M%S"))
+htmlfilename <- paste0("tbats_html_animated_plot_",format(Sys.time(),"%Y%m%d%H%M%S"),".html")
+saveHTML(ani.replay(), img.name = imgname, htmlfile = htmlfilename, outdir = getwd())
